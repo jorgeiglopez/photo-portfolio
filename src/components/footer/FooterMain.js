df1d6fd0004215a479fb1cb8b6f7df608b736e6d
jorgeiglopez <jorgeiglopez@gmail.com>
2021-07-25 11:52:06 +1000

    typo + testing git config

--- src/components/footer/FooterMain.js
+++ src/components/footer/FooterMain.js
@@ -1,7 +1,7 @@
 import classes from './FooterMain.module.css';
 
 const FooterMain = () => {
-	return <footer className={classes.footer}>I'm th footer</footer>;
+	return <footer className={classes.footer}>I'm the footer</footer>;
 };
 
 export default FooterMain;
